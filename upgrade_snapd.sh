8a273a49e9bd516425653670a45f6e9b513541bf
Svet Bajlekov <tomatoman@gmail.com>
2020-07-27 09:00:46 +0200

    apt purge usually throws error - don't stop on it

--- upgrade_snapd.sh
+++ upgrade_snapd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 wget https://gist.githubusercontent.com/svet-b/6d2f11b59e5fec4416e65f20bce49cb1/raw/bc9dc7224c509bf7037dc38119720e2d448c888f/add_raspbian_testing_repo.sh
 chmod a+x add_raspbian_testing_repo.sh
