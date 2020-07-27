723cce9ee3884170e2f38afc0561a4f21b7934c3
Svet Bajlekov <tomatoman@gmail.com>
2020-07-27 08:47:08 +0200

    Only purge after update

--- upgrade_snapd.sh
+++ upgrade_snapd.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-
-apt purge snapd -y
+#!/bin/bash -e
 
 wget https://gist.githubusercontent.com/svet-b/6d2f11b59e5fec4416e65f20bce49cb1/raw/bc9dc7224c509bf7037dc38119720e2d448c888f/add_raspbian_testing_repo.sh
 chmod a+x add_raspbian_testing_repo.sh
 ./add_raspbian_testing_repo.sh
 
+apt update
+
+apt purge snapd -y
 rm -rf /var/cache/snapd
 rm -rf /var/lib/snapd
 
-apt update
 apt install snapd/testing libseccomp2/testing -y
 
 snap install ammp-connect
